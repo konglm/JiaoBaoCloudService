cbce09944c51cffdb863408aca5007a9804220d4
konglm <klm.821222@163.com>
2016-10-28 10:13:49 +0800

    增加部分sql

--- src/main/java/com/goldeneyes/IDao/NoteMapper.java
+++ src/main/java/com/goldeneyes/IDao/NoteMapper.java
@@ -18,4 +18,18 @@ public interface NoteMapper {
     int updateByPrimaryKey(Note record);
     
     List<Note> getAllNote();
+    /**
+	 * 获取用户未读点到记事条数
+	 * @param userId
+	 * @param spaceType
+	 * @return
+	 */
+	int getNoReadNotesCntByUser(int userId,int spaceType);
+	/**
+	 * 获取用户未读点到记事列表
+	 * @param userId
+	 * @param spaceType
+	 * @return
+	 */
+	List<Note> getNoReadNotesByUser(int userId,int spaceType);
 }
